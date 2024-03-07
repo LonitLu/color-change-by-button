20555516a314e379795dd2aafe08b3221d6352b9
LonitLu <dimasik17021989@gmail.com>
2024-03-08 00:30:18 +0500

    добавил исчезание кнопок

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const colors = ["#FF0000", "#FF1493", "#2E8B57", "#87CEEB", "#696969"];
 const page = document.getElementById("container");
 const btn = document.getElementById("btn");
+const btnDel = document.getElementById("btn__none");
 
 colorResp = () => {
   const randomColors = Math.floor(Math.random() * colors.length);
@@ -9,3 +10,11 @@ colorResp = () => {
 };
 
 btn.addEventListener("click", colorResp);
+
+
+btnNone = () => {
+  btnDel.classList.add("btn__none");
+  btn.classList.add("btn__none");
+};
+
+btnDel.addEventListener("click", btnNone);
